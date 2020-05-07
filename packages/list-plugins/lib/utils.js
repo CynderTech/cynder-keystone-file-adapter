bbe15df9de064b3b350c342322fc600f7f2413ae
Paul Peria <periabytes@gmail.com>
2020-05-07 13:42:53 +0800

    removed

--- packages/list-plugins/lib/utils.js
+++
@@ -1,7 +0,0 @@
-exports.composeHook = (originalHook, newHook) => async params => {
-  let { resolvedData } = params;
-  if (originalHook) {
-    resolvedData = await originalHook(params);
-  }
-  return newHook({ ...params, resolvedData });
-};
